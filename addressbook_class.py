7136d78c3e12e1c2db591df3702b6549063e6120
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-28 09:39:06 +0100

    add logic to adressbook func

--- addressbook_class.py
+++ addressbook_class.py
@@ -5,11 +5,16 @@ from record_class import Record
 
 class AddressBook(UserDict):
 
-    def add_record(self, record: Record) -> None:
-        pass
+    def add_record(self, record: Record) -> bool:
+        self.data[record.name.value] = record
+        return True
 
     def find(self, name: str) -> Optional[Record]:
-        pass
+        return self.data.get(name)
 
     def delete(self, name: str) -> bool:
-        pass
+        if name in self.data:
+            del self.data[name]
+            return True
+        else:
+            return False
