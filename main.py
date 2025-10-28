5894434e25c372a8ccf6524fca838130fbc17b18
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-28 09:41:31 +0100

    fulfilled main for calling functions

---
+++ main.py
@@ -0,0 +1,30 @@
+from addressbook_class import AddressBook
+from record_class import Record
+from class_fields_templates import Name, Phone
+
+
+def main():
+    book = AddressBook()
+
+    john_record = Record(Name("John"))
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+    book.add_record(john_record)
+
+    jane_record = Record(Name("Jane"))
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    # Show all records
+    print("All contacts:")
+    for name, record in book.data.items():
+        print(record)
+
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)
+
+
+if __name__ == "__main__":
+    main()
