c4a126d75c8da26dc5dab824b4e11c2b0e87d874
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-28 09:42:11 +0100

    fulfilled logic in record class methods

--- record_class.py
+++ record_class.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Optional
 
 from class_fields_templates import Name, Phone
 
@@ -7,16 +7,29 @@ from class_fields_templates import Name, Phone
 @dataclass
 class Record:
     name: Name
-    phone: List[Phone] = field(default_factory=List)
+    phones: List[Phone] = field(default_factory=list)
 
     def add_phone(self, phone: str) -> None:
-        pass
+        self.phones.append(Phone(phone))
 
-    def remove_phone(self, phone: str) -> None:
-        pass
+    def find_phone(self, phone: str) -> Optional[Phone]:
+        phone_generator = (p for p in self.phones if p.value == phone)
+        return next(phone_generator, None)
 
-    def edit_phone(self, phone: str) -> None:
-        pass
+    def remove_phone(self, phone: str) -> bool:
+        phone_obj = self.find_phone(phone)
+        if phone_obj:
+            self.phones.remove(phone_obj)
+            return True
+        return False
 
-    def find_phone(self, phone: str) -> None:
-        pass
+    def edit_phone(self, old_phone: str, new_phone: str) -> bool:
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                return True
+        return False
+
+    def __str__(self):
+        phone_str = '; '.join(p.value for p in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phone_str}"
